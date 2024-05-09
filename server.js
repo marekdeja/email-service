f01f29e3f6d371858616f70c939b5e18445ff18c
deyamarco <marek.deja@yahoo.com>
2024-05-09 15:58:22 +0200

    port env

--- server.js
+++ server.js
@@ -33,4 +33,5 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-app.listen(3001, () => console.log("Server started on port 3001"));
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server started on port ${port}`));
